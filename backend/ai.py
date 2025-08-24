e4408460ddc0aedb150937230e2d16d20c34c6e0
Rithvik <vikturi99@utexas.edu>
2025-08-24 13:02:30 -0500

    Generate messages with openai secret

--- backend/ai.py
+++ backend/ai.py
@@ -1,19 +1,29 @@
 import openai
-from config import set_vars
 import os
-
-
-
+from config import set_vars
+set_vars()
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 def generate_message(name, tone="gentle"):
-    set_vars()
-    
-    openai.api_key = os.environ["OPENAI_API_KEY"]
+
     prompt = f"""
     You're an empathetic life coach calling {name}, who is feeling low.
-    Give a 60-second {tone} motivational good morning message encouraging them to get out of bed and take control of their lives. Emphasize how in control of their lives they really are.
+    Give a 30-second {tone} motivational good morning message encouraging them to get out of bed and take control of their lives. Emphasize how in control of their lives they really are.
     """
     response = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
+
+def generate_message_for_conversation(tone="gentle", user_input=""):
+    prompt = f"""
+    You're an empathetic, yet constructive life coach calling a person, who is feeling low.
+    Provide a 1-3 sentences response to the user and their story, almost like a human friend would.
+    Always end with a question to the user to continue the conversation.
+    User's story: {user_input}
+    """
+    response = openai.ChatCompletion.create(
+        model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content.strip()
